d51b9dc827c849c9220e59bfc483b77fdf47c662
Sidhant Dhyani <siddhantydhyani99@gmail.com>
2023-09-21 13:42:34 +0530

    login and register backend added

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,21 +1,59 @@
-
-import React from 'react'
-import "./Login.css"
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:4000/login", {
+        email: email,
+        password: password,
+      })
+      .then((result) => {
+        if(result.data === "Logged in successfully!!") {
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className='login'>
-        <div className='loginform_container'>
-            <h1>Login Here</h1>
-            <form action="" className='loginform'>
-                <input type="text" placeholder='Username'/>
-                <input type="text" placeholder='Password'/>
-                <button className='btn'>Register</button>
-                <p>New to TruckDekho? <a className='registerlink' href="/register">Sign-in</a></p>
-            </form>
-        </div>
+    <div className="login">
+      <div className="loginform_container">
+        <h1>Login Here</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="btn">Login</button>
+          <p>
+            New to TruckDekho?
+            <Link className="loginlink" to="/register">
+              Register
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,23 +1,66 @@
-
-import React from 'react'
-import "./Register.css"
+import React, { useState } from "react";
+import "./Register.css";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
 const Register = () => {
+  const [username, setuserName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:4000/register", {
+        username: username,
+        email: email,
+        password: password,
+      })
+      .then((result) => {
+        console.log(result);
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className='register'>
-        <div className='registerform_container'>
-            <h1>Register Here</h1>
-            <form action="localhost://4000/register" method='post' className='registerform'>
-                <input type="text" placeholder='Username'/>
-                <input type="email" placeholder='email'/>
-                <input type="password" placeholder='Password'/>
-                <input type="password" placeholder='Confirm Password'/>
-                <button className='btn'>Register</button>
-                <p>Already have an account? <a className='loginlink' href="/login">Login</a></p>
-            </form>
-        </div>
+    <div className="register">
+      <div className="registerform_container">
+        <h1>Register Here</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="username"
+            name="username"
+            onChange={(e) => setuserName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="btn">Register</button>
+          <p>
+            Already have an account?
+            <Link className="loginlink" to="/login">
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Register;
